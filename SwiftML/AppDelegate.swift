9c098d9787bc9e9ab180b73584eea2ca724c92b1
Qualan Woodard <quawood@qualans-imac.local>
2017-11-24 21:02:20 -0500

    Initial Commit

---
+++ SwiftML/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftML
+//
+//  Created by Qualan Woodard on 11/24/17.
+//  Copyright © 2017 Qualan Woodard. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
